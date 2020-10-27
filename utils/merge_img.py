6ec7cc0830f7f5c37581f8f7c736cc42a82d4225
15013412747 <496340909@qq.com>
2020-10-28 07:00:34 +0800

    merge_img

--- utils/merge_img.py
+++ utils/merge_img.py
@@ -8,11 +8,11 @@ IMAGES_FORMAT = ['.jpg', '.png']  # 图片格式
 
 # src = input('请输入图片文件路径：')
 IMAGE_SIZE = 1000  # 每张小图片的大小
-IMAGE_ROW = 4  # 图片间隔，也就是合并成一张图后，一共有几行
-IMAGE_COLUMN = 6  # 图片间隔，也就是合并成一张图后，一共有几列
+IMAGE_ROW = 30  # 图片间隔，也就是合并成一张图后，一共有几行
+IMAGE_COLUMN = 30  # 图片间隔，也就是合并成一张图后，一共有几列
 
 
-# 定义图像拼接函数
+# 定义图像拼接函数 单张图片拼接
 def image_compose(file_name, IMAGE_SAVE_PATH):
     to_image = Image.new('RGB', (IMAGE_COLUMN * IMAGE_SIZE, IMAGE_ROW * IMAGE_SIZE))  # 创建一个新图
     # to_image = Image.new('L', (IMAGE_COLUMN * IMAGE_SIZE, IMAGE_ROW * IMAGE_SIZE))
@@ -40,32 +40,32 @@ def image_compose(file_name, IMAGE_SAVE_PATH):
     return to_image.save(IMAGE_SAVE_PATH)  # 保存新图
 
 
-# 拼接一个目录下的所有图片为一张大图
+# 拼接一个目录下的所有小图片为一张大图 拼单张图片
 # 根据图片命名来自动计算行列数
 #
 def image_compose2(file_name, img_save_path):
     # 遍历所有图片
     img_list = os.listdir(file_name)
     img_total = len(img_list)
+    # print(img_total, img_list)
     img_column = 0
     for _img in img_list:
         if _img[0] == '0':
             img_column = img_column + 1
         else:
             break
-        print(_img[0], _img)
-    img_row = img_total / img_column
+        # print(_img[0], _img)
+    img_row = int(img_total / img_column)
 
     print(img_total, img_column, img_row)
 
-    return
-    to_image = Image.new('RGB', (IMAGE_COLUMN * IMAGE_SIZE, IMAGE_ROW * IMAGE_SIZE))  # 创建一个新图
+    to_image = Image.new('RGB', (img_column * IMAGE_SIZE, img_row * IMAGE_SIZE))  # 创建一个新图
     # to_image = Image.new('L', (IMAGE_COLUMN * IMAGE_SIZE, IMAGE_ROW * IMAGE_SIZE))
     # 循环遍历，把每张图片按顺序粘贴到对应位置上
     num = 0
     mnu = 0
-    for y in range(0, IMAGE_ROW):
-        for x in range(0, IMAGE_COLUMN):
+    for y in range(0, img_row):
+        for x in range(0, img_column):
 
             # path = file_name + str(num) + '_' + str(mnu) + '_' + \
             #        str(IMAGE_SIZE) + '_' + str(IMAGE_SIZE) + '.png'
@@ -73,28 +73,24 @@ def image_compose2(file_name, img_save_path):
                                 str(IMAGE_SIZE) + '_' + str(IMAGE_SIZE) + '.jpg')
             # print(path)
             # f_dir_name = os.path.split(os.path.split(os.path.split(path)[0])[0])[1]
-
             from_image = Image.open(path).resize(
                 (IMAGE_SIZE, IMAGE_SIZE), Image.ANTIALIAS)
             to_image.paste(from_image, ((x - 0) * IMAGE_SIZE, (y - 0) * IMAGE_SIZE))
             mnu += 1
-            if mnu == IMAGE_COLUMN:
+            if mnu == img_column:
                 mnu = 0
                 num += 1
 
     return to_image.save(img_save_path)  # 保存新图
 
 
-image_compose2(r"G:\pos_calculation_YiDu2\block7_jpg_cut_save",
-               r"G:\pos_calculation_YiDu3\block7_jpg_mem")
-exit()
-
-
+# 拼接所有区块
 def total_merge_img(in_path, out_path):
     in_path_list = os.listdir(in_path)
     for q_dir in in_path_list:
         # print(q_dir)
         print(os.listdir(os.path.join(in_path, q_dir)))
+        # block_merge(cut_img_path, merge_img_path)
         for img_name in os.listdir(os.path.join(in_path, q_dir)):
             print("=== img_name:", img_name)
             cut_img_path = os.path.join(os.path.join(in_path, q_dir, img_name))
@@ -104,6 +100,15 @@ def total_merge_img(in_path, out_path):
             print("cut", cut_img_path)
             # print(merge_img_path)
             merge_img2(cut_img_path, merge_img_path)
+
+
+def block_merge(block_path, merge_block_path):
+    for img_name in os.listdir(block_path):
+        print(os.path.join(block_path, img_name))
+        path1 = os.path.join(block_path, img_name)
+        path2 = os.path.join(merge_block_path, img_name) + '.jpg'
+        print(path1, path2)
+        image_compose2(path1, path2)
 
 
 def merge_img2(cut_img_path, merge_img_path):
@@ -161,6 +166,7 @@ def merge_img(cut_img_path, merge_img_path):
 if "__main__" == __name__:
     # in_path = r"F:\3bangs_qu\save_img"
     # out_path = r"F:\3bangs_qu\merge_img"
+    # block_merge(r"G:\pos_calculation_YiDu2\block7_jpg_cut_save", r"G:\pos_calculation_YiDu3\block7_jpg_mem")
     in_path = r"G:\pos_calculation_YiDu2"
     out_path = r"G:\pos_calculation_YiDu3\block7_jpg_mem"
     total_merge_img(in_path, out_path)
